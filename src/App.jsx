0dad79e7cdc5f71b813b0777a4efd6a50ac43ca5
mdieude14 <martin.dieude@gmail.com>
2025-12-17 11:47:12 +0100

    Routes + vidéo

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,37 @@
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Plat from "./components/Plat";
 
 function App() {
   return (
     <>
-      <Plat />
+      <BrowserRouter>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Acceuil</Link>
+            </li>
+            <li>
+              <Link to="/plat">Plat</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <h1>Bienvenue chez maïté</h1>
+
+        <iframe
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/8-lCVMAZBhM?si=3x3IzmiYOZ21TmdA"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerpolicy="strict-origin-when-cross-origin"
+          allowfullscreen
+        ></iframe>
+        <Routes>
+          <Route path="/plat" element={<Plat />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
